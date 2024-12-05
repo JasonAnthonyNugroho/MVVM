2ac1fe1e57524ccda5fe5ca733d73377d80526d0
itsHikariii <hasonanthonynugroho88@gmail.com>
2024-12-05 23:06:51 +0700

    Add project files.

---
+++ MauiAppProject/Data/CourseCategoryRepository.cs
@@ -0,0 +1,57 @@
+﻿using MauiAppProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MauiAppProject.Data
+{
+    public class CourseCategoryRepository : ICourseCategory
+    {
+
+        private readonly HttpClient _httpClient;
+        public CourseCategoryRepository(HttpClient httpClient) 
+        {
+            _httpClient = httpClient;
+        
+        }
+
+        public Task<CourseCategory> AddAsync(CourseCategory courseCategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CourseCategory> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<CourseCategory>> ListAsync()
+        {
+            var response = await _httpClient.GetAsync($"{HttpHelper.baseAddress}/api/v1/Categories");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var category = JsonSerializer.Deserialize<List<CourseCategory>>(content,
+                    new JsonSerializerOptions { PropertyNameCaseInsensitive = false });
+                return category ?? new List<CourseCategory>();
+
+            }
+            else {
+                throw new ArgumentException("Cannot Get Data From Server");
+            }
+        }
+
+        public Task<CourseCategory> UpdateAsync(CourseCategory courseCategory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MauiAppProject/Data/ICourseCategory.cs
@@ -0,0 +1,18 @@
+﻿using MauiAppProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppProject.Data
+{
+    public interface ICourseCategory
+    {
+        Task<List<CourseCategory>> ListAsync();
+        Task<CourseCategory> GetAsync(int id);
+        Task<CourseCategory> AddAsync(CourseCategory courseCategory);
+        Task<CourseCategory> UpdateAsync(CourseCategory courseCategory);
+        Task DeleteAsync(int id);
+    }
+}
